b52b8be00a28998d3e449f06ce33b3294bbab5fd
ReginaProkopeva <reginaprokopeva@internet.ru>
2025-01-21 09:30:09 +0300

    Commit Lesson_5 updated

--- src/ArrayProcessing.java
+++ src/ArrayProcessing.java
@@ -1,26 +1,38 @@
 public class ArrayProcessing {
     public static int processArray(String[][] array) throws MyArraySizeException, MyArrayDataException {
+        // Проверка, что массив содержит ровно 4 строки
         if (array.length != 4) {
-            throw new MyArraySizeException("Размер массива должен быть 4x4!");
+            throw new MyArraySizeException("Размер массива должен быть 4x4! Найдено строк: " + array.length);
         }
-        for (String[] row : array) {
-            if (row.length != 4) {
-                throw new MyArraySizeException("Размер массива должен быть 4x4!");
+
+
+        // Проверка, что каждая строка содержит ровно 4 элемента
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length != 4) {
+                throw new MyArraySizeException(
+                        "Размер массива должен быть 4x4! В строке " + i + " найдено элементов: " + array[i].length
+                );
             }
         }
 
 
         int sum = 0;
+
+
+        // Проход по всем элементам массива и их суммирование
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
                 try {
-                    sum += Integer.parseInt(array[i][j]);
+                    sum += Integer.parseInt(array[i][j]); // Преобразование элемента в int
                 } catch (NumberFormatException e) {
-                    throw new MyArrayDataException("Некорректные данные в ячейке [" + i + "][" + j + "]: " + array[i][j]);
+                    throw new MyArrayDataException(
+                            "Некорректные данные в ячейке [" + i + "][" + j + "]: " + array[i][j]
+                    );
                 }
             }
         }
-        return sum;
 
+
+        return sum;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        // Корректный массив
         String[][] validArray = {
                 {"1", "2", "3", "4"},
                 {"5", "6", "7", "8"},
@@ -10,13 +11,16 @@ public class Main {
         };
 
 
+        // Массив с неправильным размером
         String[][] invalidSizeArray = {
-                {"1", "2", "3"},
-                {"4", "5", "6"},
-                {"7", "8", "9"}
+                {"1", "2", "3", "4"},
+                {"5", "6", "7", "8", "3232", "123"},
+                {"9", "10", "11", "12"},
+                {"13", "14", "15", "16"}
         };
 
 
+        // Массив с некорректными данными
         String[][] invalidDataArray = {
                 {"1", "2", "3", "4"},
                 {"5", "six", "7", "8"},
@@ -33,14 +37,14 @@ public class Main {
 
 
         try {
-            System.out.println("Сумма элементов массива: " + ArrayProcessing.processArray(validArray));
+            System.out.println("Сумма элементов массива: " + ArrayProcessing.processArray(invalidSizeArray));
         } catch (MyArraySizeException | MyArrayDataException e) {
             System.err.println("Ошибка: " + e.getMessage());
         }
 
 
         try {
-            System.out.println("Сумма элементов массива: " + ArrayProcessing.processArray(validArray));
+            System.out.println("Сумма элементов массива: " + ArrayProcessing.processArray(invalidDataArray));
         } catch (MyArraySizeException | MyArrayDataException e) {
             System.err.println("Ошибка: " + e.getMessage());
         }
